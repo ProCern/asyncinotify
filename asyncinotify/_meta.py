25b4aaf155d780027c8b9d7f17f5e8b25508c7ee
Taylor C. Richberger <taywee@gmx.com>
2021-01-04 21:05:22 -0700

    version bump

--- asyncinotify/_meta.py
+++ asyncinotify/_meta.py
@@ -4,7 +4,7 @@
 
 data = dict(
     name='asyncinotify',
-    version='2.0.1',
+    version='2.0.2',
     author='Taylor C. Richberger',
     description='A simple optionally-async python inotify library, focused on simplicity of use and operation, and leveraging modern Python features',
     license='MIT',
